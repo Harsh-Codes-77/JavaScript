0c0ae19f957988b1cc9a9939decaa18005c38753
Harsh Pathak <83079207+pathak703@users.noreply.github.com>
2024-07-20 17:08:52 +0000

    conversionOperations

---
+++ basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = 50
+console.log(typeof score);
+
+let scores = "50"
+console.log(typeof scores);
+
+//Now convert the string into number
+
+let valueInNumber = Number(scores);
+console.log(typeof valueInNumber);
+
+//Convert a number into boolean
+
+let isLoggedIN = 1
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Harsh" => true
