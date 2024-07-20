548d58b3b5c126c6d98f59eb61aa4ba4df028eba
Harsh Pathak <83079207+pathak703@users.noreply.github.com>
2024-07-20 11:22:40 +0000

    dataTypes

---
+++ basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all JS code as a newer version
+//alert (3 + 3) //we are using nodejs not browser
+
+let name = "Harsh"
+let age = 18
+let isLoggedIn = false
+
+//There are so many types of dataTypes
+
+//Example --->
+
+//number => 123
+//bigint
+//string => ""
+//boolean => true/false
+//undefined =>  let x
+//null => let y = null --- standalone value
+//sumbol => let value = Symbol ('unique)
+//object => let person = (name: 'Harsh')
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
