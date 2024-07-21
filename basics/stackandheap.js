1ab09b53a85aad2eaa023950dbb267974420210b
Harsh Pathak <83079207+pathak703@users.noreply.github.com>
2024-07-21 10:37:54 +0000

    stackandheap

---
+++ basics/stackandheap.js
@@ -0,0 +1,18 @@
+let myName = "Yash"
+let anotherNAme = myName
+anotherNAme = "Harsh"
+
+console.log(myName);
+console.log(anotherNAme);
+
+let userOne = {
+    email : "harsh@gmail.com",
+    upi : "harsh@ybl"
+}
+
+console.log(userOne);
+
+let userTwo = userOne
+userTwo.email = "harsh@google.com"
+
+console.log(userTwo);
