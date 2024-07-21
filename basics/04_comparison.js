cff5271d5261f9a87cecae5ac10cb95b0e9b072c
Harsh Pathak <83079207+pathak703@users.noreply.github.com>
2024-07-21 10:05:07 +0000

    comparisons

---
+++ basics/04_comparison.js
@@ -0,0 +1,11 @@
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2==1);
+//console.log(2!=1);
+
+console.log(null>0); //output = false
+console.log(null==0); //output = false
+console.log(null>=0); //output = true
+
+//Comparison convert null to a number treating it as 0. That's why null >= 0 is true and null > 0 is false.
